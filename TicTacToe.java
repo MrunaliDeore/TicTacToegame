dd712e4bdf5b9bb682d0ccd66978ebd47dbf40c2
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-05 12:32:18 +0530

    Added : create fresh tic tac toe board

---
+++ TicTacToe.java
@@ -0,0 +1,39 @@
+package TicTacToeCode;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TicTacToe {
+	static Scanner in;
+	static String[] board;
+	static String turn;
+
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		board = new String[9];
+		turn = "X";
+		String winner = null;
+		populateEmptyBoard();
+
+		System.out.println("Welcome to 2 Player In Tic Tac Toe Game.");
+		System.out.println("--------------------------------");
+		printBoard();
+	}
+
+	static void printBoard() {
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\");
+	}
+
+	static void populateEmptyBoard() {
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a+1);
+		}
+	}
+}
