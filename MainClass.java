df144db77c1589d26f2a4392f481dd1412ccf2da
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-05 12:28:58 +0530

    Added: main clas file - simple welcome msg here

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+package TicTacToeCode;
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Assignment No. 18");
+		System.out.println("\nTic-Tac-Toe Board game");
+	}
+}
